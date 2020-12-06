5816ed7b276b1f5252e8677eefec89200ab6687e
Bela <belahamid@gmail.com>
2020-12-06 17:15:34 +0000

    added route '/winner/rock/scissors'

--- app/controller.py
+++ app/controller.py
@@ -1,9 +1,14 @@
 from app import app
+from app.models.games import *
 
 
 @app.route('/')
 def index():
     return "Welcome to Rock, Paper, Scissors!"
+
+@app.route('/winner/rock/scissors')
+def winner():
+    return f"{player1.name} chose {player1.move} and is the winner!"
 
 
 
